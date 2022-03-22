1c7ba4143b88cbe23c4fa465f86f52f75b9a665c
Claudio O <56867330+kruemelbacke@users.noreply.github.com>
2022-03-22 14:32:03 +0100

    init card generator

---
+++ card_generator.py
@@ -0,0 +1,13 @@
+"""
+This is a script to generate 3x4 card matrix
+"""
+class Card_Field():
+    """
+    A class to represent the playing field filled with cards
+    """
+    def __init__(self):
+        self.__cards = []
+
+
+    def generate_cards():
+        pass
