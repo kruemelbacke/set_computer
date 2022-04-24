f5afa3a7d2955672c0ab80c6cc83e15f17a0a96f
Claudio O <56867330+kruemelbacke@users.noreply.github.com>
2022-04-24 12:12:06 +0200

    converted GUI into object oriented

--- gui_engine.py
+++ gui_engine.py
@@ -1,55 +1,73 @@
 """
 blblblblbl
 """
-
 import pygame
 
 
-pygame.init()
+class GUI():
+    def __init__(self):
+        """
+        Inits GUI
+        """
+        pygame.init()
 
-clock = pygame.time.Clock()
+        self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+        pygame.display.set_caption("SET! Game")
 
-res = (720,720)
+        self.clock = pygame.time.Clock()
 
-screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+        self.color = (255,255,255)
 
-color = (255,255,255)
+        self.color_light = (170,170,170)
 
-color_light = (170,170,170)
+        self.color_dark = (100,100,100)
 
-color_dark = (100,100,100)
+        self.width = self.screen.get_width()
 
-width = screen.get_width()
+        self.height = self.screen.get_height()
 
-height = screen.get_height()
+        self.smallfont = pygame.font.SysFont('Corbel',35)
 
-smallfont = pygame.font.SysFont('Corbel',35)
+        self.text = self.smallfont.render('Quit' , True , self.color)
 
-text = smallfont.render('Quit' , True , color)
+        self.game_active = True
 
-while True:
+    def run(self):
+        """
+        Run GUI
+        """
 
-    for ev in pygame.event.get():
+        while self.game_active:
 
-        if ev.type == pygame.QUIT:
-            pygame.quit()
+            for ev in pygame.event.get():
+                self.mouse = pygame.mouse.get_pos()
 
-        if ev.type == pygame.MOUSEBUTTONDOWN:
+                if ev.type == pygame.QUIT:
+                    self.game_active = False
 
-            if width/2 <= mouse[0] <= width/2+140 and height/2 <= mouse[1] <= height/2+40:
-                pygame.quit()
+                if ev.type == pygame.MOUSEBUTTONDOWN:
 
-    screen.fill((60,25,60))
+                    if (self.width/2 <= self.mouse[0] <= self.width/2+140
+                        and self.height/2 <= self.mouse[1] <= self.height/2+40):
+                        self.game_active = False
 
-    mouse = pygame.mouse.get_pos()
+            self.screen.fill((60,25,60))
 
-    if width/2 <= mouse[0] <= width/2+140 and height/2 <= mouse[1] <= height/2+40:
-        pygame.draw.rect(screen,color_light,[width/2,height/2,140,40])
+            # check if mouse is on button
+            if (self.width/2 <= self.mouse[0] <= self.width/2+140
+                and self.height/2 <= self.mouse[1] <= self.height/2+40):
+                pygame.draw.rect(self.screen,self.color_light,[self.width/2,self.height/2,140,40])
 
-    else:
-        pygame.draw.rect(screen,color_dark,[width/2,height/2,140,40]) 
+            else:
+                pygame.draw.rect(self.screen,self.color_dark,[self.width/2,self.height/2,140,40]) 
 
-    screen.blit(text , (width/2+50,height/2))
+            self.screen.blit(self.text , (self.width/2+50,self.height/2))
 
-    pygame.display.update()
-    clock.tick(60)
+            pygame.display.update()
+            self.clock.tick(60)
+
+        pygame.quit()
+
+if __name__ == '__main__':
+    myGUI = GUI()
+    myGUI.run()
