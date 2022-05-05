7de8f538a6c548177ae1ad088a53f8fb08a948d1
Claudio O <56867330+kruemelbacke@users.noreply.github.com>
2022-05-05 12:19:44 +0200

    act path

--- test_camera.py
+++ test_camera.py
@@ -4,4 +4,4 @@ camera = PiCamera()
 camera.resolution = (2592, 1944 )
 for i in range(10):
 
-    camera.capture('/home/pi/Desktop/image%s.jpg' % i)
+    camera.capture('/home/claudipi/Pictures/image%s.jpg' % i)
