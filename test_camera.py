79dafbb6db4357c5ca1fdd2d27f11d8965374832
kruemelbacke <mail@claudio-oechsler.de>
2022-05-05 12:44:03 +0200

    penislol

--- test_camera.py
+++ test_camera.py
@@ -5,6 +5,6 @@ try:
     camera.resolution = (2592, 1944 )
     for i in range(10):
 
-        camera.capture_continuous('/home/claudipi/Bilder/image%s.jpg' % i)
+        camera.capture('/home/claudipi/Bilder/image%s.jpg' % i)
 finally:
     camera.close()
