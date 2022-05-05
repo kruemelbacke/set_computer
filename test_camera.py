9ef90005de894795d629ad6374160e44ab9c29ca
Claudio O <56867330+kruemelbacke@users.noreply.github.com>
2022-05-05 12:40:51 +0200

    added sleep to test camera

--- test_camera.py
+++ test_camera.py
@@ -1,8 +1,10 @@
+from time import sleep
 from picamera import PiCamera
 
 camera = PiCamera()
 try:
     camera.resolution = (2592, 1944 )
+    sleep(1)
     for i in range(10):
 
         camera.capture_continuous('/home/claudipi/Bilder/image%s.jpg' % i)
