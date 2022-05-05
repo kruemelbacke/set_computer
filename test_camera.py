3420568d54e1d1bca03290cea249953070927a00
Claudio O <56867330+kruemelbacke@users.noreply.github.com>
2022-05-05 12:30:25 +0200

    added camera close statement

--- test_camera.py
+++ test_camera.py
@@ -1,7 +1,10 @@
 from picamera import PiCamera
 
 camera = PiCamera()
-camera.resolution = (2592, 1944 )
-for i in range(10):
+try:
+    camera.resolution = (2592, 1944 )
+    for i in range(10):
 
-    camera.capture('/home/claudipi/Bilder/image%s.jpg' % i)
+        camera.capture('/home/claudipi/Bilder/image%s.jpg' % i)
+finally:
+    camera.close()
