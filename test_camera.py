cf982dc59543c2816534a2540265a92ce60164ec
Claudio O <56867330+kruemelbacke@users.noreply.github.com>
2022-08-04 12:55:38 +0200

    bugfix: delay for starting camera

--- test_camera.py
+++ test_camera.py
@@ -1,10 +1,14 @@
 from camera_stream import CameraStream
 import cv2 as cv
+import time
 
 
 CamStream = CameraStream(res=(1280, 720), fps=10)
 CamStream.start_stream()
 
+time.sleep(3)
+
 while True:
     img = CamStream.get()
     cv.imshow('Camera Test', img)
+    time.sleep(1)
