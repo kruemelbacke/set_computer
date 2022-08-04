ada61933bc411807d78883f753bccba8be95c6ce
Claudio O <56867330+kruemelbacke@users.noreply.github.com>
2022-08-04 12:59:41 +0200

    bugfix: waitKey fun added

--- test_camera.py
+++ test_camera.py
@@ -11,4 +11,11 @@ time.sleep(3)
 while True:
     img = CamStream.get()
     cv.imshow('Camera Test', img)
+
+    key = cv.waitKey(1) & 0xFF
+
+    # if the `q` key was pressed, break from the loop
+    if key == ord("q"):
+        break
+
     time.sleep(1)
