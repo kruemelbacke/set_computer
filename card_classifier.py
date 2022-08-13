3c7dc9153ffce193595af99c07cf2256744b9cd9
Claudio O <56867330+kruemelbacke@users.noreply.github.com>
2022-08-13 13:58:39 +0200

    ajusted lightness thresh

--- card_classifier.py
+++ card_classifier.py
@@ -107,7 +107,7 @@ class CCardClassifier:
             cv.FONT_HERSHEY_SIMPLEX, 0.8, (255,0,0), 2)
         if saturation_hsv > 0.5:
             card.attributes["shading"] = "solid"
-        elif lightness_hls > 0.9:
+        elif lightness_hls > 0.98:
             card.attributes["shading"] = "empty"
         else:
             card.attributes["shading"] = "hatched"
