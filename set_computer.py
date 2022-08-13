fcd35e4ef0180593a7c5d55578af4562b9fb583b
Claudio O <56867330+kruemelbacke@users.noreply.github.com>
2022-08-13 14:18:28 +0200

    showd shading

--- set_computer.py
+++ set_computer.py
@@ -158,7 +158,7 @@ if __name__ == '__main__':
         else:
             cv.imshow("CardDetection", cv.resize(img_raw, (WIN_BIG_W, WIN_BIG_H)))
 
-        # show_img_from_cards(Cards, "warp_symbol_center_boxes", "Shading detection")
+        show_img_from_cards(Cards, "warp_symbol_center_boxes", "Shading detection")
         # show_img_from_cards([Cards[0]], "warp", "Flatten")
         # show_img_from_cards([Cards[0]], "warp_grey", "Flatten grey")
         # show_img_from_cards([Cards[0]], "warp_thresh", "Flatten threshold")
