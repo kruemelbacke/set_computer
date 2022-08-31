b326aa18d1cd405f1887b429050ee0ca3fcd0c78
Claudio O <56867330+kruemelbacke@users.noreply.github.com>
2022-08-31 11:53:29 +0200

    if color is to dark assert it to be purple

--- card_classifier.py
+++ card_classifier.py
@@ -135,7 +135,7 @@ class CCardClassifier:
         min_value = np.amin(warp_hsv[:, :, 2])
 
         if min_value < 0.15:
-            card.attributes["color"] = "unknown"
+            card.attributes["color"] = "purple"
         else:
             card.attributes["color"] = max_color
 
