87c489d0eef19b3789bd8b625c829be1683c22e8
Claudio O <56867330+kruemelbacke@users.noreply.github.com>
2022-09-19 16:16:02 +0200

    debug

--- card_detection.py
+++ card_detection.py
@@ -38,11 +38,11 @@ class CQueryCard(set_engine.CCard):
         """Flattens an img_raw of a card into a top-down 200x300 perspective."""
         FLATTEN_WIDTH = 200
         FLATTEN_HEIGHT = 300
-
+        print(pts)
         temp_rect = np.zeros((4, 2), dtype="float32")
-
+        
         summ = np.sum(pts, axis=2)
-
+        print(summ)
         tl = pts[np.argmin(summ)]
         br = pts[np.argmax(summ)]
 
